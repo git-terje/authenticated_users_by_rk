31e95b472642c831b242c2dfdb706a4adf03f1cd
Terje R <t.ronning@gmail.com>
2025-09-05 23:24:42 +0200

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,1 +1,44 @@
-
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+        changelogTitle: "# Changelog"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "custom_components/authenticated_users_by_rk/manifest.json",
+          "hacs.json",
+          "CHANGELOG.md",
+          "package.json"
+        ],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "authenticated_users_by_rk.zip",
+            label: "HACS Install Package"
+          }
+        ]
+      }
+    ]
+  ],
+  prepare: [
+    {
+      path: "@semantic-release/exec",
+      cmd: "sed -i 's/\"version\": \".*\"/\"version\": \"${nextRelease.version}\"/' custom_components/authenticated_users_by_rk/manifest.json hacs.json"
+    }
+  ]
+};
